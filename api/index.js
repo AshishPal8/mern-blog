7e400b3b3d1b0b1e17854c0191d7868bb3ee5023
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-05-02 07:16:41 +0530

    update user api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "../api/routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -16,14 +18,14 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
+app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
-// middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,5 @@
 import { Button, TextInput } from "flowbite-react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const DashProfile = () => {
